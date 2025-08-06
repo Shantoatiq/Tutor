1f6a376826be54294c793ba0b5df2f077befd8d1
Shantoatiq <149778777+Shantoatiq@users.noreply.github.com>
2025-08-06 07:59:11 +0600

    Add files via upload

--- main.asset.php
+++ main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'c09aa4f447e29e3ff6bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '56c944b544d4248ff44d');
